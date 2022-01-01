549c4343d30eda5abf46447f8806cbdf5502c9b6
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-01-01 13:13:26 +0300

    Update setup.py and setup.cfg for PyPI

--- setup.py
+++ setup.py
@@ -9,4 +9,18 @@ setup(
     author_email="ymzymz2007@gmail.com",
     license='MIT',
     install_requires=['google-api-python-client', 'pycountry', 'httplib2', 'typing'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )
