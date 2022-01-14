bbe90678d9c07bec2df4ae3a4216e03dbd26df45
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-01-14 20:19:40 +0300

    Update version information

--- perspective/__init__.py
+++ perspective/__init__.py
@@ -9,7 +9,7 @@ __title__ = "perspective.py"
 __author__ = "Yilmaz04"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Yilmaz Alpaslan"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from .main import Client
 from .attributes import Attributes
