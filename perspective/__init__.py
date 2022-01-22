797ff60ac04530958d467ef2475046b78dddfaeb
Yilmaz4 <ymzymz2007@gmail.com>
2022-01-22 13:33:12 +0300

    Update license year

--- perspective/__init__.py
+++ perspective/__init__.py
@@ -8,7 +8,7 @@ An easy-to-use API wrapper for Perspective API written in Python.
 __title__ = "perspective.py"
 __author__ = "Yilmaz04"
 __license__ = "MIT"
-__copyright__ = "Copyright 2021 Yilmaz Alpaslan"
+__copyright__ = "Copyright 2021-2022 Yilmaz Alpaslan"
 __version__ = "0.3.4"
 
 from .main import Client
