e789bede5579a6f0f7968cfdad7139379e2c4d21
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2023-09-30 10:58:00 +0300

    Update attributes.py

--- perspective/attributes.py
+++ perspective/attributes.py
@@ -1,26 +1,8 @@
-"""
-MIT License
-
-Copyright (c) 2017-2022 Yilmaz Alpaslan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+__title__ = "perspective.py"
+__author__ = "Yilmaz4"
+__license__ = "MIT"
+__copyright__ = "Copyright © 2017-2023 Yilmaz Alpaslan"
+__version__ = "1.0.0"
 
 all_attrs = ["TOXICITY", "SEVERE_TOXICITY", "IDENTITY_ATTACK", 
 "INSULT", "PROFANITY", "THREAT", "TOXICITY_EXPERIMENTAL", 
